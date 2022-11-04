112d79e8172563d99bfd29f496b8eb647352c429
Tijana <tijana88iv@gmail.com>
2022-11-04 10:52:54 +0100

    Excaption handling homework

---
+++ HomeWork-2/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace HomeWork_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.lblR = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblP = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(28, 23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(341, 159);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnDiv);
+            this.tabPage1.Controls.Add(this.btnSum);
+            this.tabPage1.Controls.Add(this.lblR);
+            this.tabPage1.Controls.Add(this.lblResult);
+            this.tabPage1.Controls.Add(this.txtNum2);
+            this.tabPage1.Controls.Add(this.txtNum1);
+            this.tabPage1.Controls.Add(this.lblNum2);
+            this.tabPage1.Controls.Add(this.lblNum1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.ForeColor = System.Drawing.Color.DarkRed;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(333, 133);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sum";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(256, 13);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(71, 23);
+            this.btnSum.TabIndex = 6;
+            this.btnSum.Text = "Sum";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Location = new System.Drawing.Point(134, 87);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(0, 16);
+            this.lblR.TabIndex = 5;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(6, 87);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(55, 16);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Result:";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(137, 49);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(137, 14);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 2;
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(6, 52);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(118, 16);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "Second Number";
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(6, 20);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(95, 16);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "First Number";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblA);
+            this.tabPage2.Controls.Add(this.lblP);
+            this.tabPage2.Controls.Add(this.txtRadius);
+            this.tabPage2.Controls.Add(this.btnCalculate);
+            this.tabPage2.Controls.Add(this.lblArea);
+            this.tabPage2.Controls.Add(this.lblPerimeter);
+            this.tabPage2.Controls.Add(this.lblRadius);
+            this.tabPage2.Font = new System.Drawing.Font("Franklin Gothic Heavy", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage2.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(333, 133);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Calculate";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(103, 84);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(0, 17);
+            this.lblA.TabIndex = 6;
+            // 
+            // lblP
+            // 
+            this.lblP.AutoSize = true;
+            this.lblP.Location = new System.Drawing.Point(103, 51);
+            this.lblP.Name = "lblP";
+            this.lblP.Size = new System.Drawing.Size(0, 17);
+            this.lblP.TabIndex = 5;
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(74, 12);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 22);
+            this.txtRadius.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(209, 12);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(94, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(6, 84);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(41, 17);
+            this.lblArea.TabIndex = 2;
+            this.lblArea.Text = "Area:";
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.AutoSize = true;
+            this.lblPerimeter.Location = new System.Drawing.Point(6, 51);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(74, 17);
+            this.lblPerimeter.TabIndex = 1;
+            this.lblPerimeter.Text = "Perimeter:";
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(6, 12);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(52, 17);
+            this.lblRadius.TabIndex = 0;
+            this.lblRadius.Text = "Radius";
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(256, 47);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(71, 23);
+            this.btnDiv.TabIndex = 7;
+            this.btnDiv.Text = "Div";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 220);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblP;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnDiv;
+    }
+}
+
---
+++ HomeWork-2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Homework";
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            decimal sum;
+            try
+            {
+                decimal num1 = Convert.ToDecimal(txtNum1.Text);
+                decimal num2 = Convert.ToDecimal(txtNum2.Text);
+                sum = num1 + num2;
+                lblR.Text = sum.ToString();
+            }
+
+            catch (System.FormatException ex)
+            {
+                MessageBox.Show(String.Format("Please enter numbers! (Exception caught: {0} at {1})", ex.Message, DateTime.Now), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double r;
+            r = Convert.ToDouble(txtRadius.Text);
+            if (r == 0)
+            {
+                MessageBox.Show("The circle does not exist");
+            }
+            else
+            {
+                double area = r * r * Math.PI;
+                area = Math.Round(area, 2);
+                double perimeter = 2 * r * Math.PI;
+                perimeter = Math.Round(perimeter, 2);
+                lblA.Text = area.ToString();
+                lblP.Text = perimeter.ToString();
+            }
+
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            decimal div = 0;
+            try
+            {
+                decimal num1 = Convert.ToDecimal(txtNum1.Text);
+                decimal num2 = Convert.ToDecimal(txtNum2.Text);
+                if (num2 == 0)
+                {
+                    throw new MyDivideByZeroException("Divide by zero is not posible");
+                }
+                div = num1 / num2;
+            }
+            catch (MyDivideByZeroException ex)
+            {
+                MessageBox.Show(String.Format("{0} (Exception caught at {1})", ex.Message, DateTime.Now), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (System.FormatException ex)
+            {
+                MessageBox.Show(String.Format("Please enter numbers! (Exception caught: {0} at {1})", ex.Message, DateTime.Now), "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally
+            {
+                if (div != 0)
+                {
+                    div = Math.Round(div, 2);
+                    lblR.Text = div.ToString();
+                }
+                else
+                {
+                    lblR.Text = "";
+                }
+
+            }
+        }
+    }
+}
